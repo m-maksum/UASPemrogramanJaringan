cd33f140a3088cce70b1f79814aca0c22bff5d1a
Agung362389010 <agung.wicaksono.362389010@gmail.com>
2024-01-24 16:41:03 +0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -10,3 +10,36 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.awt.Color;
+
+public class Server {
+
+  private int port;
+  private List<User> clients;
+  private ServerSocket server;
+
+  public static void main(String[] args) throws IOException {
+    new Server(12345).run();
+  }
+
+  public Server(int port) {
+    this.port = port;
+    this.clients = new ArrayList<User>();
+  }
+
+  public void run() throws IOException {
+    server = new ServerSocket(port);
+    System.out.println("Port 12345 is now open.");
+
+    try {
+      while (!server.isClosed()) {
+        // accepts a new client
+        Socket client = server.accept();
+
+        // get nickname of newUser
+        String nickname = (new Scanner ( client.getInputStream() )).nextLine();
+        nickname = nickname.replace(",", ""); //  ',' use for serialisation
+        nickname = nickname.replace(" ", "_");
+        System.out.println("New Client: \"" + nickname + "\"\n\t     Host:" + client.getInetAddress().getHostAddress());
+
+        // create new User
+        User newUser = new User(client, nickname);
