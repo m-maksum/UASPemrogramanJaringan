5f4d47b473b9447e5019e5017ef82c2e011d2767
Muhammad Maksum <149471329+m-maksum@users.noreply.github.com>
2024-01-24 19:14:48 +0700

    Add files via upload

---
+++ ClientGui.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.html.*;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.filechooser.FileFilter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
